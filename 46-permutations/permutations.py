f06b709329341cfff8809872877cc3c8f75608cf
iammeghana7 <51442814+iammeghana7@users.noreply.github.com>
2023-10-15 09:51:35 -0400

    Time: 52 ms (13.37%) | Memory: 16.5 MB (25.21%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,17 +1,22 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        ans = []
-        def backtracking(path):
-            if len(path) == len(nums):
-                # print("path is ",path,ans)
-                ans.append(path[:])
-                # print("Now ans is ",ans)
-                return 
-            for n in nums:
-                if n not in path:
-                    path.append(n)
-                    backtracking(path)
-                    path.pop()
-        backtracking([])
-        return ans
-            
+        res = []
+        cur = []
+        def dfs(cur):
+            if len(cur) == len(nums):
+                res.append(cur[:])
+            for i in range(len(nums)):
+                if nums[i] not in cur:
+                    cur.append(nums[i])
+                    dfs(cur)
+                    cur.pop()
+        for i in range(len(nums)):
+
+            cur.append(nums[i])
+            # print("Befor starting dfs cur is ",cur)
+            dfs(cur)
+            # print("AFter first iteration res is ",res)
+            cur = []
+        # print("Res is ",res)
+        return res
+        
