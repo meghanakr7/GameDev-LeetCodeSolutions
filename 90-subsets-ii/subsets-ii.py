7d5f6cd1ee4fb43ff74ede00df21b92a193f3b0d
iammeghana7 <51442814+iammeghana7@users.noreply.github.com>
2023-10-25 10:08:21 -0400

    Time: 38 ms (85.16%) | Memory: 16.5 MB (35.10%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -5,15 +5,12 @@ class Solution:
         cur = []
         nums.sort()
         def dfs(ind, cur):
-            print("CUr is ",ind,cur,indexes)
-            
             if cur not in res:
                 res.append(cur[:])
             if len(cur) == len(nums):
                 return
             for i in range(ind, len(nums)):
                 if i not in indexes:
-                    print("I is not in indexes ",i,indexes,cur)
                     indexes.append(i)
                     cur.append(nums[i])
                     dfs(i +1, cur)
@@ -22,12 +19,8 @@ class Solution:
         for i in range(len(nums)):
             indexes.append(i)
             cur.append(nums[i])
-            print("Index and cur are ",indexes, cur)
             dfs(i, cur)
             indexes = []
             cur = []
-        # if nums not in res:
-        #     res.append(nums)
-        res.append([])
-        print("Res is ",res)  
+        res.append([]) 
         return res
