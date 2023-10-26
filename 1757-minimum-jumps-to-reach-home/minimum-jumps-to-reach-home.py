81b8a8af0fdee2912fa1424b5b97c72123c31805
iammeghana7 <51442814+iammeghana7@users.noreply.github.com>
2023-10-26 15:10:21 -0400

    Time: 266 ms (10.62%) | Memory: 17 MB (73.29%) - LeetSync

---
+++ 1757-minimum-jumps-to-reach-home/minimum-jumps-to-reach-home.py
@@ -0,0 +1,27 @@
+forb = 0
+class Solution:
+    def minimumJumps(self, forbidden: List[int], a: int, b: int, x: int) -> int:
+        limit = 2000 + a + b
+        visited = set(forbidden)
+        queue = deque([(0, True)])
+        hops = 0
+        while queue:
+            l = len(queue)
+            while l>0:
+                l -= 1
+                print("Queue is ",queue)
+                pos, isForward = queue.popleft()
+                if pos == x :
+                    return hops
+                if pos in visited:
+                    continue
+                visited.add(pos)
+                if isForward == True:
+                    if pos - b >= 0:
+                        queue.append(((pos-b), False))
+                if pos + a <= limit:
+                    queue.append(((pos+a), True))
+            hops += 1
+                
+        return -1
+            
