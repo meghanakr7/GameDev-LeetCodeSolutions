a58e758b1501f1ffd05a8c0c31f3aea547b5ff8e
iammeghana7 <51442814+iammeghana7@users.noreply.github.com>
2023-10-27 09:38:05 -0400

    Time: 514 ms (65.09%) | Memory: 30.5 MB (96.13%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.py
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.py
@@ -2,17 +2,19 @@ class Solution:
     def peakIndexInMountainArray(self, arr: List[int]) -> int:
         low = 0
         high = len(arr) - 1
-        maxi = -math.inf
-        maxiInd = -1
+        mid = 0
+        n = len(arr)
         while low <= high:
-            if arr[low] > maxi:
-                maxi = arr[low]
-                maxiInd = low
-            if arr[high] > maxi:
-                maxi = arr[high]
-                maxiInd = high
-            low += 1
-            high -= 1
-        return maxiInd
+            mid = (low + high)//2
+            print("mid is ",mid, arr[mid], arr[mid-1], arr[mid+1])
+            if ((mid == 0 or arr[mid-1]) < arr[mid] and (mid == n-1 or arr[mid+1]) < arr[mid]):
+                return mid
+            if (mid == 0 or arr[mid - 1] < arr[mid]) and (mid == n - 1 or arr[mid + 1] < arr[mid]):
+                return mid
+            elif mid > 0 and arr[mid-1] > arr[mid]:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return -1
         
         
