b93bde6e977aa85133eaf15f23e7d9c6bea9eb23
iammeghana7 <51442814+iammeghana7@users.noreply.github.com>
2023-10-27 09:38:37 -0400

    Time: 520 ms (46.20%) | Memory: 30.9 MB (55.77%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.py
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.py
@@ -6,10 +6,7 @@ class Solution:
         n = len(arr)
         while low <= high:
             mid = (low + high)//2
-            print("mid is ",mid, arr[mid], arr[mid-1], arr[mid+1])
             if ((mid == 0 or arr[mid-1]) < arr[mid] and (mid == n-1 or arr[mid+1]) < arr[mid]):
-                return mid
-            if (mid == 0 or arr[mid - 1] < arr[mid]) and (mid == n - 1 or arr[mid + 1] < arr[mid]):
                 return mid
             elif mid > 0 and arr[mid-1] > arr[mid]:
                 high = mid - 1
