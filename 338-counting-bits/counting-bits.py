9d39cbfc0d69302fd74a0ec55cef249b14f5bc2d
iammeghana7 <51442814+iammeghana7@users.noreply.github.com>
2023-11-04 13:36:05 -0400

    Time: 77 ms (50.73%) | Memory: 23.2 MB (8.25%) - LeetSync

---
+++ 338-counting-bits/counting-bits.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        ones = [0]*(n+1)
+        for i in range(1, n+1):
+            ones[i] = ones[i//2] + (i)%2
+        print("Ones i is ",ones)
+        return ones
+
+        
