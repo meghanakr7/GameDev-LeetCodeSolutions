3e00ca4ac4be1eff0b87c0b0bb8d2d80d755e06d
iammeghana7 <51442814+iammeghana7@users.noreply.github.com>
2023-11-05 09:17:38 -0500

    Time: 74 ms (63.37%) | Memory: 16.5 MB (27.58%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -2,18 +2,19 @@ class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         m = len(image)
         n = len(image[0])
-        print("m and n are ",m,n)
-        s = set()
-        def bfs(i, j):
-            if i < 0 or i >= m or j < 0 or j >= n or image[i][j] != oldColor or (i,j) in s:
-                print("i came here")
-                return 
-            if (i,j) not in s:
-                s.add((i,j))
-            if image[i][j] == oldColor:
-                image[i][j] = color
-            return bfs(i-1, j) or bfs(i, j-1) or bfs(i, j+1) or bfs(i+1,j)
         oldColor = image[sr][sc]
-        bfs(sr, sc)
-        print("Grid now is ",image)
+        visited = set()
+        def bfs(r, c, oldColor):
+            # print("r and c are", r,c)
+            if r < 0 or r > m-1 or c < 0 or c > n-1 or ((r, c) in visited) or image[r][c] != oldColor:
+                return
+            visited.add((r,c))
+            if image[r][c] == oldColor:
+                image[r][c] = color
+            return bfs(r-1, c, oldColor) or bfs(r, c-1, oldColor) or bfs(r, c+1, oldColor) or bfs(r+1, c, oldColor)
+        # visited.add((sr, sc))
+        # image[sr][sc] = color
+        bfs(sr,sc,oldColor)
+        # print("image is ",image)
         return image
+        
