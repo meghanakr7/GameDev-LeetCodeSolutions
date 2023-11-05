dc06203e50bc7885be9b0caaa7fd6906ef15d034
iammeghana7 <51442814+iammeghana7@users.noreply.github.com>
2023-11-05 17:58:53 -0500

    Time: 889 ms (5.17%) | Memory: 22.3 MB (35.00%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
@@ -2,33 +2,26 @@ class Solution:
     def shipWithinDays(self, weights: List[int], days: int) -> int:
         left = max(weights)
         right = sum(weights)
-        appropriate_weight = 0
-        def feasible(mid):
-            d = 0
-            i = 0
-            computed_weight = 0
-            while i < len(weights):
-                s = 0
-                while i < len(weights) and s + weights[i]<= mid:
+        def isFeasible(mid):
+            d = 1
+            s = 0
+            # print("mid is ",mid)
+            for i in range(len(weights)):
+                if s + weights[i] <= mid:
                     s += weights[i]
-                    computed_weight += weights[i]
-                    i += 1
-                print("s value for day is ",s)
-                d += 1
-            print("i and d are ",i,d,computed_weight)
-            if computed_weight == sum(weights) and d <= days:
+                    # print("weights and s is ",weights[i],s)
+                else:
+                    print("till now s is ",s)
+                    s  = weights[i]
+                    d += 1
+            # print("d is ",d)
+            if d <= days:
                 return True
             return False
         while left < right:
-            appropriate_weight = left + (right - left)//2
-            print("approporate weight is ",left, right, appropriate_weight)
-            if feasible(appropriate_weight):
-                right = appropriate_weight
+            mid = left + (right - left) // 2
+            if isFeasible(mid):
+                right = mid
             else:
-                left = appropriate_weight + 1
-            # print("mid, left, right",appropriate_weight, left,right)
+                left = mid + 1
         return left
-        
-            
-
-        
