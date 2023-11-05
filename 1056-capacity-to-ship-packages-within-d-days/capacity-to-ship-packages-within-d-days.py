e02c684af8fa5e5685df69453ad4095c336fa18e
iammeghana7 <51442814+iammeghana7@users.noreply.github.com>
2023-11-05 17:59:06 -0500

    Time: 612 ms (22.09%) | Memory: 22.1 MB (49.53%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
@@ -5,16 +5,12 @@ class Solution:
         def isFeasible(mid):
             d = 1
             s = 0
-            # print("mid is ",mid)
             for i in range(len(weights)):
                 if s + weights[i] <= mid:
                     s += weights[i]
-                    # print("weights and s is ",weights[i],s)
                 else:
-                    print("till now s is ",s)
                     s  = weights[i]
                     d += 1
-            # print("d is ",d)
             if d <= days:
                 return True
             return False
