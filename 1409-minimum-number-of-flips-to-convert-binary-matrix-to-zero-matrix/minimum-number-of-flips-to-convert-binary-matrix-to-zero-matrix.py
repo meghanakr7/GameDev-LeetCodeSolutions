bc610569d2808d70358f8fa76a3e0537bb86b6f1
iammeghana7 <51442814+iammeghana7@users.noreply.github.com>
2023-11-05 10:00:21 -0500

    Time: 134 ms (20.00%) | Memory: 16.6 MB (29.71%) - LeetSync

--- 1409-minimum-number-of-flips-to-convert-binary-matrix-to-zero-matrix/minimum-number-of-flips-to-convert-binary-matrix-to-zero-matrix.py
+++ 1409-minimum-number-of-flips-to-convert-binary-matrix-to-zero-matrix/minimum-number-of-flips-to-convert-binary-matrix-to-zero-matrix.py
@@ -1,37 +1,37 @@
 class Solution:
     def minFlips(self, mat: List[List[int]]) -> int:
-        queue = deque()
-        visited = set()
-        queue.append((mat, 0))
-        visited.add(tuple(tuple(row) for row in mat))
-        def flip(row, col, state):
-            ans = deepcopy(state)
-            ans[row][col] ^= 1
-            if row - 1 >= 0:
-                ans[row-1][col] ^= 1
-            if row + 1 < len(state):
-                ans[row+1][col] ^= 1
-            if col - 1 >= 0:
-                ans[row][col-1] ^= 1
-            if col + 1 < len(state[0]):
-                ans[row][col+1] ^= 1
+        isFinalState = True
+        q = deque([])
+        q.append((tuple(tuple(row for row in mat)), 0))
+        def flip(r, c, temp):
+            ans = deepcopy(temp)
+            ans[r][c] ^= 1
+            if r + 1 < len(ans):
+                ans[r+1][c] ^=1
+            if r - 1 >= 0:
+                ans[r-1][c] ^= 1
+            if c + 1 < len(ans[0]):
+                ans[r][c+1] ^= 1
+            if c - 1 >= 0:
+                ans[r][c-1] ^= 1
             return ans
-        
-        while queue:
-            isFinal = True
-            curState, numSteps = queue.popleft()
-            # print("CurState is ",curState)
-            for row in range(len(curState)):
-                for col in range(len(curState[0])):
-                    if curState[row][col] == 1:
-                        isFinal = False
+        visited = set()
+        while q:
+            isFinalState = True
+            tempState, numSteps = q.popleft()
+            for row in range(len(tempState)):
+                for col in range(len(tempState[0])):
+                    if tempState[row][col] == 1:
+                        isFinalState = False
                         break
-            if isFinal:
+            if isFinalState:
                 return numSteps
-            for row in range(len(curState)):
-                for col in range(len(curState[0])):
-                    tempState = flip(row, col, curState)
-                    if tuple(tuple(row) for row in tempState) not in visited:
-                        queue.append((tempState, numSteps + 1))
-                        visited.add(tuple(tuple(row) for row in tempState))
+            for row in range(len(mat)):
+                for col in range(len(mat[0])):
+                    t = flip(row, col, tempState)
+                    if (tuple(tuple(row) for row in t)) not in visited:
+                        q.append((t, numSteps + 1))
+                        visited.add(tuple(tuple(row) for row in t))
         return -1
+
+        
