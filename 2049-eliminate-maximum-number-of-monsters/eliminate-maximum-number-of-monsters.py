38b016295cebba749e613efdf98e43666658906d
iammeghana7 <51442814+iammeghana7@users.noreply.github.com>
2023-11-06 20:41:30 -0500

    Time: 694 ms (68.73%) | Memory: 32.2 MB (62.27%) - LeetSync

---
+++ 2049-eliminate-maximum-number-of-monsters/eliminate-maximum-number-of-monsters.py
@@ -0,0 +1,17 @@
+class Solution:
+    def eliminateMaximum(self, dist: List[int], speed: List[int]) -> int:
+        monsters = 0
+        charged = 1
+        dead = 0
+        t = 0
+        n = len(dist)
+        time = [0] * n
+        monsters = 0
+        for i in range(len(dist)):
+            time[i] = dist[i] / speed[i]
+        time = sorted(time)
+        for i in range(len(time)):
+            if time[i] <= i:
+                break
+            monsters += 1
+        return monsters
