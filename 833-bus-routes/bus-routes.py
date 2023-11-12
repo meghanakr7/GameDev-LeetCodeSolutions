e371346df3b498d370ff09e755148e8371d7e3db
iammeghana7 <51442814+iammeghana7@users.noreply.github.com>
2023-11-12 18:11:46 -0500

    Time: 424 ms (99.38%) | Memory: 32 MB (99.05%) - LeetSync

--- 833-bus-routes/bus-routes.py
+++ 833-bus-routes/bus-routes.py
@@ -2,20 +2,16 @@ class Solution:
     def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
         if source == target:
             return 0
-
         max_stop = max(max(route) for route in routes)
-        if max_stop < target:
+        if target > max_stop:
             return -1
-
-        n = len(routes)
-        min_buses_to_reach = [float('inf')] * (max_stop + 1)
-        min_buses_to_reach[source] = 0
-
         flag = True
+        min_buses_to_reach = [math.inf] * (max_stop + 1)
+        min_buses_to_reach[source] = 0
         while flag:
             flag = False
             for route in routes:
-                mini = float('inf')
+                mini = math.inf
                 for stop in route:
                     mini = min(mini, min_buses_to_reach[stop])
                 mini += 1
@@ -23,8 +19,4 @@ class Solution:
                     if min_buses_to_reach[stop] > mini:
                         min_buses_to_reach[stop] = mini
                         flag = True
-
-        return min_buses_to_reach[target] if min_buses_to_reach[target] < float('inf') else -1
-            
-
-        
+        return min_buses_to_reach[target] if min_buses_to_reach[target] < math.inf else -1
