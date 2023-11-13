1a4a2e72e5ab46a10e41cf63fab3f190343aa845
iammeghana7 <51442814+iammeghana7@users.noreply.github.com>
2023-11-12 20:57:07 -0500

    Time: 171 ms (41.49%) | Memory: 19.6 MB (38.63%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.py
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.py
@@ -8,14 +8,12 @@ class Solution:
         t = []
         vowels.sort()
         k = 0
-        print("Sorted vowels are ",vowels)
         for i in range(len(s)):
             if s[i] not in actualVowels:
                 t.append(s[i])
             else:
                 t.append(vowels[k])
                 k += 1
-        print("Final t is ",t,"".join(t))
         return "".join(t)
 
 
