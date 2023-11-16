e72fa93a9a4e425433d4f93be137365ffe685876
iammeghana7 <51442814+iammeghana7@users.noreply.github.com>
2023-11-16 11:26:11 -0500

    Time: 48 ms (10.74%) | Memory: 16.3 MB (35.31%) - LeetSync

--- 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.py
+++ 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.py
@@ -6,15 +6,12 @@ class Solution:
         count = 0
         while j < len(s):
             goodString.append(s[j])
-            print("after appending ",goodString)
             if j - i + 1 == 3:
                 if len(set(goodString)) == 3:
                     count += 1
                 goodString = goodString[1:len(goodString)]
-                print("now good string is ",goodString)
                 i += 1
             j += 1
-        print("count is ",count)
         return count
 
         
