995ce23a7e3c2a6d479adda8a885d19ca79c7aa8
iammeghana7 <51442814+iammeghana7@users.noreply.github.com>
2023-11-19 11:54:38 -0500

    Time: 38 ms (91.62%) | Memory: 16.6 MB (15.97%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,7 +1,6 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         res = []
-        cur = []
         def dfs(cur):
             if len(cur) == len(nums):
                 res.append(cur[:])
@@ -11,12 +10,5 @@ class Solution:
                     dfs(cur)
                     cur.pop()
         for i in range(len(nums)):
-
-            cur.append(nums[i])
-            # print("Befor starting dfs cur is ",cur)
-            dfs(cur)
-            # print("AFter first iteration res is ",res)
-            cur = []
-        # print("Res is ",res)
+            dfs([nums[i]])
         return res
-        
