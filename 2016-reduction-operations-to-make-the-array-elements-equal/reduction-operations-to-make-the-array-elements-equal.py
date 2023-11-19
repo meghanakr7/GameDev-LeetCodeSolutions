fa92c020b27e33c5bb927779b8e43673766533cf
iammeghana7 <51442814+iammeghana7@users.noreply.github.com>
2023-11-19 10:52:03 -0500

    Time: 883 ms (25.77%) | Memory: 28.2 MB (21.65%) - LeetSync

--- 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.py
+++ 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.py
@@ -3,18 +3,15 @@ class Solution:
         nums.sort()
         n = len(nums)
         d = {}
-        numsSet = list(set(nums)) # Duplicates
+        numsSet = list(set(nums))
         numsSet.sort()
+        s = 0
         for i in range(len(nums)):
             if nums[i] in d:
                 d[nums[i]] += 1
             else:
                 d[nums[i]] = 1
-        s = 0
         keys = list(d.keys())
-        # print("nums srot ",sorted(nums))
-        # print("keys are ",keys)
-        print("d is ",d)
         for i in range(1, len(keys)):
             s += i * d[keys[i]]
         return s
