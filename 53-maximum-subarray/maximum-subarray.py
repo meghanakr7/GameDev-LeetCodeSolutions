7dca264201294cc1e9499a5df6918e1dbd3fa2c1
iammeghana7 <51442814+iammeghana7@users.noreply.github.com>
2023-11-22 18:25:38 -0500

    Time: 620 ms (33.68%) | Memory: 30.6 MB (19.72%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,9 +1,15 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        dynamicTable = [0] * len(nums)
-        dynamicTable[0] = nums[0]
-        for i in range(1, len(nums)):
-            dynamicTable[i] = max(nums[i], dynamicTable[i-1] + nums[i])
-        # print("dynamic Table is ",dynamicTable)
-        # print("max is ",max(dynamicTable))
-        return max(dynamicTable)
+        i = 0
+        j = 0
+        currentSum = 0
+        maxi = -math.inf
+        while j < len(nums):
+            if (currentSum < 0 and nums[j] >= currentSum):
+                i = j
+                currentSum = 0
+            currentSum += nums[j]
+            maxi = max(maxi, currentSum)
+            j += 1
+        return maxi
+        
