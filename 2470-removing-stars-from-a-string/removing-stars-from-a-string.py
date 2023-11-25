5f2f7f2bbce57d35834e8230ef0d1c72b616159e
iammeghana7 <51442814+iammeghana7@users.noreply.github.com>
2023-11-25 18:40:25 -0500

    Time: 254 ms (26.51%) | Memory: 17.9 MB (44.65%) - LeetSync

--- 2470-removing-stars-from-a-string/removing-stars-from-a-string.py
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.py
@@ -3,13 +3,9 @@ class Solution:
         stack = []
         for i in range(len(s)):
             if s[i] == '*':
-                # print("Befor stack is ",stack)
                 stack.pop()
-                # print("After that stack is ",stack)
             else:
                 stack.append(s[i])
-        # print("stack is ",stack)
-        # print("str is ", "".join(stack))
         res = "".join(stack)
         return res
 
