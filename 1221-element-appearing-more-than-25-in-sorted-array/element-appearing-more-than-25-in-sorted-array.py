781b41291bb24b68bea9f931e2fa4d8afc51677f
iammeghana7 <51442814+iammeghana7@users.noreply.github.com>
2023-12-10 19:02:53 -0500

    Time: 1110 ms (10.52%) | Memory: 18.3 MB (5.85%) - LeetSync

--- 1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.py
+++ 1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.py
@@ -2,7 +2,6 @@ class Solution:
     def findSpecialInteger(self, arr: List[int]) -> int:
         quarter = len(arr)*(0.25)
         unique = list(set(arr))
-        print('quarter is ',quarter)
         for i in range(len(unique)):
             if arr.count(unique[i]) > quarter:
                 return unique[i]
