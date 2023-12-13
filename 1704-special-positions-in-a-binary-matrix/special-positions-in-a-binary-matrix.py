4106057915bc87006e56683b575a1c7f2c2028c2
iammeghana7 <51442814+iammeghana7@users.noreply.github.com>
2023-12-13 11:34:00 -0500

    Time: 164 ms (32.42%) | Memory: 16.8 MB (33.33%) - LeetSync

---
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.py
@@ -0,0 +1,25 @@
+class Solution:
+    def numSpecial(self, mat: List[List[int]]) -> int:
+        m, n = len(mat), len(mat[0])
+        def checkRow(row, col):
+            for i in range(m):
+                if i!=row and mat[i][col] == 1:
+                    return False
+            return True
+        def checkCol(row, col):
+            for j in range(n):
+                if j!=col and mat[row][j] == 1:
+                    return False
+            return True
+    
+        finalRes = 0
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] == 1:
+                    if (checkRow(i, j) and checkCol(i, j)):
+                        finalRes += 1
+        print('finalRes ',finalRes)
+        return finalRes
+
+
+                    
