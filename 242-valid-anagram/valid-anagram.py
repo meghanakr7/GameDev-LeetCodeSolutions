4e2c45f7143282c310215b53cd3b4676d5cb63c1
iammeghana7 <51442814+iammeghana7@users.noreply.github.com>
2023-12-15 19:27:45 -0500

    Time: 55 ms (52.83%) | Memory: 17 MB (32.14%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,9 +1,25 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        d = defaultdict()
-        if len(s) != len(t):
-            return False
-        for i in range(len(t)):
-            if t.count(t[i]) != s.count(t[i]):
+        d = {}
+        e = {}
+        for i in range(26):
+            d[chr(ord('a')+i)] = 0
+            e[chr(ord('a')+i)] = 0
+        for char in s:
+            if char not in d:
+                d[char] = 0
+            d[char] += 1
+        for char in t:
+            if char not in e:
+                e[char] = 0
+            e[char] += 1
+        for i in range(26):
+            print('val is ',ord('a'))
+            print('val is ',chr(ord('a')+i))
+            # if chr(ord('a')+i) in d and chr(ord('a')+i) in e:
+            if d[chr(ord('a')+i)] != e[chr(ord('a')+i)]:
                 return False
         return True
+            
+
+        
