6f33ed1c000e66a2ff4b97688f832d3b78be2988
iammeghana7 <51442814+iammeghana7@users.noreply.github.com>
2023-12-18 16:19:02 -0500

    Time: 591 ms (72.61%) | Memory: 30.6 MB (32.09%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -4,7 +4,5 @@ class Solution:
         dp[0] = nums[0]
         for i in range(1, len(nums)):
             dp[i] = max(dp[i-1] + nums[i], nums[i])
-        print('dp is ',dp)
-        print('max is ',max(dp))
         return max(dp)
         
