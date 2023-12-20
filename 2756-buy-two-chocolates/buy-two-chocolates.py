b5c16958b98dd4ce5e8c3112acb7446862e2ba17
iammeghana7 <51442814+iammeghana7@users.noreply.github.com>
2023-12-20 03:23:33 -0500

    Time: 58 ms (88.58%) | Memory: 17.4 MB (23.41%) - LeetSync

--- 2756-buy-two-chocolates/buy-two-chocolates.py
+++ 2756-buy-two-chocolates/buy-two-chocolates.py
@@ -4,19 +4,14 @@ class Solution:
         secSmallest = math.inf
         findex = -1
         for i in range(len(prices)):
-            print('price i is ',prices[i])
             if prices[i] < firstSmallest:
                 findex = i
                 secSmallest = firstSmallest
-                firstSmallest = prices[i]
-                
+                firstSmallest = prices[i] 
             if prices[i] < secSmallest and secSmallest >= firstSmallest and i!=findex:
                 secSmallest = prices[i]
-            print('after loop first and sec are ',firstSmallest, secSmallest)
-        print('first and sec ',firstSmallest, secSmallest)
         if money - (firstSmallest + secSmallest) >= 0:
             return money - (firstSmallest + secSmallest)
-        else:
-            return money
+        return money
 
         
