e5e88220977bd1e0ddbe5492d799831ad80c0188
iammeghana7 <51442814+iammeghana7@users.noreply.github.com>
2024-01-02 08:39:16 -0500

    Time: 145 ms (5.01%) | Memory: 17.6 MB (5.17%) - LeetSync

--- 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.py
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.py
@@ -1,33 +1,21 @@
 class Solution:
     def findMatrix(self, nums: List[int]) -> List[List[int]]:
-        count = defaultdict(int)
-        maxNum = 0
-        for i in range(len(nums)):
-            if nums[i] not in count:
-                count[nums[i]] = nums.count(nums[i])
-            if maxNum < nums.count(nums[i]):
-                maxNum = nums.count(nums[i])
-        # print("count is ",count,maxNum)
-        i = 1
-        sol = []
+        s = set()
+        count = len(nums)
         res = []
-        while i <= maxNum:
-            sol = []
-            for j in count.keys():
-                # print("val of j and i is ",j,i)
-                if count[j] >= i:
-                    sol.append(j)
-            # print("Sol is ",sol)
-            res.append(sol)
-            i += 1
-        # print("Res is ",res)
+        while count:
+            s = set()
+            for i in range(len(nums)):
+                s.add(nums[i])
+            print('s is ',s)
+            for i in list(s):
+                nums.remove(i)
+            res.append(list(s))
+            print('nums is ',nums)
+            count = len(nums)
+        print('res is ',res)
         return res
-            
-
-
-        # for i in range(len(count)):
-
-
         
 
+            
         
