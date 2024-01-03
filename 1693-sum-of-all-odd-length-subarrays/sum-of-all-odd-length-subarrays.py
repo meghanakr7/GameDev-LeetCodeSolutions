8cf848b231534f479baff08e59da7b686da8f04f
iammeghana7 <51442814+iammeghana7@users.noreply.github.com>
2024-01-02 21:27:38 -0500

    Time: 88 ms (18.38%) | Memory: 17.5 MB (5.39%) - LeetSync

--- 1693-sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.py
+++ 1693-sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.py
@@ -1,12 +1,21 @@
 class Solution:
     def sumOddLengthSubarrays(self, arr: List[int]) -> int:
-        length = 1
-        totalSum = 0
-        while length <= len(arr):
-            for i in range(len(arr)):
-                if ((i + length - 1) < len(arr)):
-                    totalSum += sum(arr[i:i+length])
-            length += 2
-        return totalSum
-
-
+        ans = sum(arr)
+        window = 3
+        max_window = len(arr) if len(arr)%2 == 1 else len(arr)-1
+        cur = 0
+        print('initial ans ',ans)
+        while window <= max_window:
+            localSum = sum(arr[:window])
+            ans += localSum
+            print('ans is ',ans)
+            left = arr[0]
+            for i in range(1, len(arr) - window + 1):
+                curr = arr[i + window - 1]
+                localSum += curr - left
+                left = arr[i]
+                ans += localSum
+            window += 2
+        print('ans ',ans)
+        return ans
+            
