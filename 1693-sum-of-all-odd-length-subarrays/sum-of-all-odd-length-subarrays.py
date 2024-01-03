1aeb1702f21e20216d8310f066eb28ea80efb711
iammeghana7 <51442814+iammeghana7@users.noreply.github.com>
2024-01-02 21:27:46 -0500

    Time: 90 ms (17.83%) | Memory: 17.4 MB (11.20%) - LeetSync

--- 1693-sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.py
+++ 1693-sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.py
@@ -4,11 +4,9 @@ class Solution:
         window = 3
         max_window = len(arr) if len(arr)%2 == 1 else len(arr)-1
         cur = 0
-        print('initial ans ',ans)
         while window <= max_window:
             localSum = sum(arr[:window])
             ans += localSum
-            print('ans is ',ans)
             left = arr[0]
             for i in range(1, len(arr) - window + 1):
                 curr = arr[i + window - 1]
@@ -16,6 +14,5 @@ class Solution:
                 left = arr[i]
                 ans += localSum
             window += 2
-        print('ans ',ans)
         return ans
             
