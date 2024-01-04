47d657f4638d839c42dbdcb9466fd05f919b62f3
iammeghana7 <51442814+iammeghana7@users.noreply.github.com>
2024-01-04 11:54:41 -0500

    Time: 45 ms (25.95%) | Memory: 17.3 MB (7.90%) - LeetSync

--- 1514-minimum-value-to-get-positive-step-by-step-sum/minimum-value-to-get-positive-step-by-step-sum.py
+++ 1514-minimum-value-to-get-positive-step-by-step-sum/minimum-value-to-get-positive-step-by-step-sum.py
@@ -1,21 +1,11 @@
 class Solution:
     def minStartValue(self, nums: List[int]) -> int:
-        mini = min(nums)
-        if mini > 0:
-            return 1
-        startVal =  1
-        
-        while True:
-            false = 0
-            prevVal = startVal
-            for i in range(len(nums)):
-                if(prevVal + nums[i] < 1):
-                    startVal += 1
-                    false = 1
-                    break
-                else:
-                    prevVal = prevVal + nums[i]
-            if not false:
-                print('start is ',startVal)
-                return startVal
-        
+        minReq = nums[0]
+        total = 0
+        for i in range(len(nums)):
+            total += nums[i]
+            minReq = min(minReq ,total)
+        if(minReq < 0):
+            minReq = abs(minReq) + 1
+            return minReq
+        return 1
