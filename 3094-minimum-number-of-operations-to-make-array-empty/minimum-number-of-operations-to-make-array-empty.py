6dc0262a351972e01bc683ee581ef0e575b4b965
iammeghana7 <51442814+iammeghana7@users.noreply.github.com>
2024-01-03 20:47:15 -0500

    Time: 559 ms (92.42%) | Memory: 31.6 MB (14.85%) - LeetSync

--- 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.py
+++ 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.py
@@ -7,7 +7,6 @@ class Solution:
                 d[nums[i]] = 1
             else:
                 d[nums[i]] += 1
-        print('d is ',d)
         for key, val in d.items():
             if val == 1:
                 return -1
@@ -18,12 +17,9 @@ class Solution:
             elif val == 4:
                 totalCount += 2
             else:
-                print('iam here')
                 num = val
                 while(num > 0 and (num%3 != 0 or (val - num) % 2 != 0)):
                     num -= 1
-                    print('new nums is ',num, num%3!=0, (val-num)%2!=0)
-                print('sum is ',num//3, (val - num)%2)
                 totalCount += (num//3) + (val - num)//2
         return totalCount
 
