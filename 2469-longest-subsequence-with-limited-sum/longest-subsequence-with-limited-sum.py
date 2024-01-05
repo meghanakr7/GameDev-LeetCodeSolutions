2d2803432eb34b32e0f66b9ebd1a6922d44a8561
iammeghana7 <51442814+iammeghana7@users.noreply.github.com>
2024-01-05 14:57:03 -0500

    Time: 160 ms (36.74%) | Memory: 17.6 MB (23.26%) - LeetSync

---
+++ 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.py
@@ -0,0 +1,24 @@
+class Solution:
+    def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
+        nums.sort()
+        n = len(nums)
+        prefixSum = [0] * n
+        prefixSum[0] = nums[0]
+        res = []
+        for j in range(1, n):
+            prefixSum[j] = nums[j] + prefixSum[j-1]
+        
+        for i in range(len(queries)):
+            enter = 0
+            for j in range(n):
+                if prefixSum[j] > queries[i]:
+                    enter = 1
+                    res.append(j)
+                    break
+            if(enter == 0):
+                res.append(n)
+                
+
+            
+        print('res is ',res)
+        return res
