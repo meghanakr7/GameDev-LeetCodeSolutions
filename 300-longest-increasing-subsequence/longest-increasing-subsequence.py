d8f7b6bc1946d6e6e02decf9209c4549c764b703
iammeghana7 <51442814+iammeghana7@users.noreply.github.com>
2024-01-05 14:37:08 -0500

    Time: 1028 ms (73.70%) | Memory: 17.7 MB (14.87%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -4,6 +4,7 @@ class Solution:
         dp = [1] * n
         for i in range(n):
             for j in range(i):
-                if(nums[i] > nums[j] and dp[i] < dp[j]+1):
-                    dp[i] = dp[j]+1
+                if nums[i] > nums[j] and dp[i] < dp[j] + 1:
+                    dp[i] = dp[j] + 1
         return max(dp)
+        
