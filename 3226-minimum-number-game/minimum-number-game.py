a992d616f73addfd0c65915bb2c3002a04fac914
iammeghana7 <51442814+iammeghana7@users.noreply.github.com>
2024-01-24 10:13:46 -0500

    Time: 38 ms (99.45%) | Memory: 16.6 MB (97.34%) - LeetSync

---
+++ 3226-minimum-number-game/minimum-number-game.py
@@ -0,0 +1,18 @@
+class Solution:
+    def numberGame(self, nums: List[int]) -> List[int]:
+        i = 0
+        n = len(nums)
+        nums.sort()
+        output = []
+        while i < n:
+            first = nums[0]
+            if nums[1]:
+                sec = nums[1]
+                output.append(sec)
+            output.append(first)
+            nums = nums[2:]
+            i += 2
+        print('output is ',output)
+        return output
+
+        
