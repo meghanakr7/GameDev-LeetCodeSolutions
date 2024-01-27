5a5412c4a66c9dbd22f5d12d6ed30919f260f1d9
iammeghana7 <51442814+iammeghana7@users.noreply.github.com>
2024-01-27 10:52:51 -0500

    Time: 185 ms (46.25%) | Memory: 24.3 MB (68.54%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -0,0 +1,27 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = 1
+        allRes = []
+        z = 0
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                z += 1
+            else:
+                res *= nums[i]
+        print('res is ',res)
+        if z >1 :
+            return [0]*len(nums)
+        for i in range(len(nums)):
+            if nums[i] !=0:
+                if z == 1:
+                    allRes.append(0)
+                else:
+                    allRes.append(int(res / nums[i]))
+            else:
+                allRes.append(res)
+        print('allRees ',allRes)
+        return allRes
+
+
+
+        
