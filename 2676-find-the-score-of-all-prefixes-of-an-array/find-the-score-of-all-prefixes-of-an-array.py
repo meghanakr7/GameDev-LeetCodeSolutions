2a6a59a8b25d163d2924889d85250b96655aab00
iammeghana7 <51442814+iammeghana7@users.noreply.github.com>
2024-01-28 13:43:42 -0500

    Time: 388 ms (63.28%) | Memory: 40.7 MB (25.25%) - LeetSync

--- 2676-find-the-score-of-all-prefixes-of-an-array/find-the-score-of-all-prefixes-of-an-array.py
+++ 2676-find-the-score-of-all-prefixes-of-an-array/find-the-score-of-all-prefixes-of-an-array.py
@@ -6,10 +6,8 @@ class Solution:
         for i in range(1, len(nums)):
             maxi = max(maxi, nums[i])
             convArr.append(nums[i] + maxi)
-        print('convArr is ',convArr)
         res = []
         res.append(convArr[0])
         for i in range(1, len(nums)):
             res.append(convArr[i] + res[-1])
-        print('res is ', res)
         return res
