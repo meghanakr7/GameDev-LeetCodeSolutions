285bccb16e8a7da611fc09631ad0c472cb7eb5ee
iammeghana7 <51442814+iammeghana7@users.noreply.github.com>
2024-01-29 09:25:53 -0500

    Time: 455 ms (5.01%) | Memory: 20 MB (72.99%) - LeetSync

---
+++ 2508-maximum-sum-of-an-hourglass/maximum-sum-of-an-hourglass.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxSum(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        hourGlass = []
+        maxi = -1
+        for i in range(m-2):
+            for j in range(n-2):
+                print('values are ',grid[i][j], grid[i][j+1], grid[i][j+2], grid[i+1][j+1], grid[i+2][j], grid[i+2][j+1], grid[i+2][j+2])
+                print(sum([grid[i][j], grid[i][j+1], grid[i][j+2], grid[i+1][j+1], grid[i+2][j], grid[i+2][j+1], grid[i+2][j+2]]))
+                maxi = max(maxi, sum([grid[i][j], grid[i][j+1], grid[i][j+2], grid[i+1][j+1], grid[i+2][j], grid[i+2][j+1], grid[i+2][j+2]]))
+        print('hourglass ',maxi)
+        return maxi
+
+
+        
