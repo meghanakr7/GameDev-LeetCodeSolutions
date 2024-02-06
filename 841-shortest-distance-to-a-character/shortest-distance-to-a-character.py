020494de2ee25315e4f23d570f51b72bc8e33d43
iammeghana7 <51442814+iammeghana7@users.noreply.github.com>
2024-02-06 08:23:11 -0500

    Time: 41 ms (81.18%) | Memory: 16.7 MB (57.49%) - LeetSync

--- 841-shortest-distance-to-a-character/shortest-distance-to-a-character.py
+++ 841-shortest-distance-to-a-character/shortest-distance-to-a-character.py
@@ -4,7 +4,6 @@ class Solution:
         for i in range(len(s)):
             if s[i] == c:
                 allIndices.append(i)
-        print('allIndices are ',allIndices)
         minR = []
         start = 10001
         end = 10001
@@ -13,19 +12,16 @@ class Solution:
             start = allIndices[0]
             end = allIndices[1]
             k = 2
-            print('start end ',start,end)
+            # print('start end ',start,end)
         if(len(allIndices) == 1):
             start = allIndices[0]
         for i in range(len(s)):
-            print('start, end in inf ',start,end,i)
             minR.append(min(abs(start-i), abs(end -i)))
-
             if i >= end:
                 start = end
                 if k < len(allIndices):
                     end = allIndices[k]
                 k +=1
-        print('minR ',minR)
         return minR
 
 
