ac8eeb705d1a2dbe082339b955e03e04941e3f98
iammeghana7 <51442814+iammeghana7@users.noreply.github.com>
2024-02-09 13:20:09 -0500

    Time: 44 ms (50.15%) | Memory: 16.6 MB (75.38%) - LeetSync

--- 862-find-and-replace-in-string/find-and-replace-in-string.py
+++ 862-find-and-replace-in-string/find-and-replace-in-string.py
@@ -1,18 +1,14 @@
 class Solution:
     def findReplaceString(self, s: str, indices: List[int], sources: List[str], targets: List[str]) -> str:
         words = list(s)
-        print('words are ',words)
         for i in range(len(indices)):
             tempWord = s[indices[i]: len(sources[i]) + indices[i]]
             tempLen = len(sources[i])
-            print('tempWord ',tempWord)
             if(tempWord == sources[i]):
                 words[indices[i]] = targets[i]
                 while tempLen != 1:
                     tempindex = indices[i]+tempLen-1
                     words[tempindex] = ''
                     tempLen -= 1
-                
-        print('words are ',words, "".join(words))
         return "".join(words)
 
