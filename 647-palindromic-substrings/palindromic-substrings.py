72d63d5b9d7f764d21e69bb880020ffd953c6da6
iammeghana7 <51442814+iammeghana7@users.noreply.github.com>
2024-02-10 20:24:39 -0500

    Time: 492 ms (8.59%) | Memory: 213.1 MB (5.51%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.py
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -2,20 +2,16 @@ class Solution:
     def countSubstrings(self, s: str) -> int:
         res = []
         count = 0
-        def palindromeCheck(s):
-            if s == s[::-1]:
-                return True
-        def generateSubstrings(s,index):
+        def createSubStrings(index):
             for i in range(len(s[index:])):
                 res.append(s[index:index+i+1])
-                
 
         for i in range(len(s)):
-            generateSubstrings(s, i)
+            createSubStrings(i)
+        # print('res is ',res)
         for i in range(len(res)):
-            if palindromeCheck(res[i]):
+            if(res[i] == res[i][::-1]):
                 count += 1
-        # print('res is ',res,count)
-        return count
-            
         
+        print('Count is ',count)
+        return count
