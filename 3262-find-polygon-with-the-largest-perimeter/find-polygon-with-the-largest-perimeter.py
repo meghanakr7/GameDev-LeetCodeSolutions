16c101c72e8d003601de78600708d5a74b6f82a6
iammeghana7 <51442814+iammeghana7@users.noreply.github.com>
2024-02-15 01:09:42 -0500

    Time: 530 ms (58.92%) | Memory: 31.2 MB (99.65%) - LeetSync

--- 3262-find-polygon-with-the-largest-perimeter/find-polygon-with-the-largest-perimeter.py
+++ 3262-find-polygon-with-the-largest-perimeter/find-polygon-with-the-largest-perimeter.py
@@ -3,12 +3,6 @@ class Solution:
         nums.sort(reverse = True)
         if len(nums) < 2:
             return -1
-        # if len(nums) == 3:
-        #     if(nums[0] + nums[1] < nums[2]):
-        #         return -1
-        #     else:
-        #         return sum(nums)
-        print(nums)
         i = 0
         total = 0
         while i < len(nums):
@@ -17,7 +11,6 @@ class Solution:
                 total = nums[i] + sum(nums[i+1:])
                 break
             i += 1
-        print('total is ',total)
         if(total == 0):
             return -1
         return total
