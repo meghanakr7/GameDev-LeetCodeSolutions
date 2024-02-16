41c74d74f9086145f501d7e5c632538fb3b83ebd
iammeghana7 <51442814+iammeghana7@users.noreply.github.com>
2024-02-16 09:39:29 -0500

    Time: 53 ms (58.30%) | Memory: 16.6 MB (60.84%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
@@ -14,13 +14,6 @@ class Solution:
                 d[num] += 1
         while '_' in nums:
             nums.remove('_')
-        # print('nums is ',nums, d[num])
-        # print('indexes ',indexes)
-        # for i in range(len(indexes)):
-            
-        #     nums.remove(nums[indexes[i]])
-        #     print('next nums ',nums)
-        print('final nums ',nums, len(nums))
         return len(nums)
 
         
