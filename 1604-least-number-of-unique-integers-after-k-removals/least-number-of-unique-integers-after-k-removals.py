49509456db0434bb5cbdeaaa57f075983052f872
iammeghana7 <51442814+iammeghana7@users.noreply.github.com>
2024-02-15 20:59:57 -0500

    Time: 385 ms (45.08%) | Memory: 42.6 MB (6.64%) - LeetSync

--- 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.py
+++ 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.py
@@ -6,13 +6,9 @@ class Solution:
                 d[arr[i]] = 0
             d[arr[i]] += 1
         d = dict(sorted(d.items(), key = lambda x:x[1]))
-        print('d is ',d)
         i = 0
-        print('d keys ',d.keys())
         keys = list(d.keys())
-        print(keys)
         count = 0
-        print('keys are ',keys)
         while k:
             if k >= d[keys[i]]:
                 k -= d[keys[i]]
@@ -20,6 +16,5 @@ class Solution:
             else:
                 break
             i += 1
-        print('count is ',count,len(keys)-count)
         return len(keys) - count
 
