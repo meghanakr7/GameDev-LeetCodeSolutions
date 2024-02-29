5220e09ecf856b63d7a087e697230c4ab2e9033a
meghanakr <51442814+meghanakr7@users.noreply.github.com>
2024-02-28 21:53:28 -0500

    Time: 368 ms (10.08%) | Memory: 67.4 MB (5.76%) - LeetSync

--- 1731-even-odd-tree/even-odd-tree.py
+++ 1731-even-odd-tree/even-odd-tree.py
@@ -6,42 +6,34 @@
 #         self.right = right
 class Solution:
     def isEvenOddTree(self, root: Optional[TreeNode]) -> bool:
-        def bfs(root):
-            q = deque([root])
-            nextValues = []
-            nextNodes = []
-            depth = 0
-            if root.val % 2 == 0:
-                return False
-            while q:
-                nextNodes = deque([])
-                nextValues = []
-               
-                for x in range(len(q)):
-                    node = q.popleft()
-                    if node:
-                        if node.left:
-                            nextNodes.append(node.left)
-                            nextValues.append(node.left.val)
-                        if node.right:
-                            nextNodes.append(node.right)
-                            nextValues.append(node.right.val)
-                depth += 1
-                # print("nextValues ",nextValues, set(nextValues), sorted(set(nextValues)), depth)
-                for i in range(len(nextValues)):
-                    if nextValues[i]%2 == depth % 2:
-                        print("nextValues and depth is ",nextValues[i], depth)
-                        return False
-                if depth%2 == 1 and sorted(set(nextValues), reverse = True) != nextValues:
-                    print("nextValues, ",nextValues, sorted(set(nextValues)),sorted(set(nextValues), reverse = True) )
-                    print("in line 31")
+        d = defaultdict(list)
+        def bfs(root,level):
+            if root is None:
+                return True
+            print(root.val)
+            d[level].append(root.val)
+            if level% 2 == 0:
+                if(root.val%2 == 0):
+                    print('here in if ',root.val)
                     return False
-                if depth%2 == 0 and sorted(set(nextValues)) != nextValues:
-                    print("sorted ",sorted(set(nextValues)), nextValues, sorted(nextValues))
-                    print("in line 34")
+            else:
+                if(root.val%2 == 1):
+                    print('here in right ',root.val)
                     return False
-                q, nextNodes = nextNodes, q
-            return True
-        return bfs(root)
-
             
+            return bfs(root.left, level + 1) and bfs(root.right, level + 1)
+        if not bfs(root,0):
+            print('here')
+            return False
+        print('d is ',d)
+        for i in range(len(d)):
+            if len(d[i]) != len(set(d[i])):
+                return False
+            temp = d[i]
+            if i%2 == 0:
+                if(sorted(temp) != d[i]):
+                    return False
+            else:
+                if(sorted(temp,reverse = True) != d[i]):
+                    return False
+        return True
