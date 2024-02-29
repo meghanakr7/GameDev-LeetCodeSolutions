883fc735c449dc2f207fec15f5d121db1cd47f77
meghanakr <51442814+meghanakr7@users.noreply.github.com>
2024-02-28 21:53:57 -0500

    Time: 275 ms (20.58%) | Memory: 67.4 MB (5.76%) - LeetSync

--- 1731-even-odd-tree/even-odd-tree.py
+++ 1731-even-odd-tree/even-odd-tree.py
@@ -10,22 +10,16 @@ class Solution:
         def bfs(root,level):
             if root is None:
                 return True
-            print(root.val)
             d[level].append(root.val)
             if level% 2 == 0:
                 if(root.val%2 == 0):
-                    print('here in if ',root.val)
                     return False
             else:
                 if(root.val%2 == 1):
-                    print('here in right ',root.val)
                     return False
-            
             return bfs(root.left, level + 1) and bfs(root.right, level + 1)
         if not bfs(root,0):
-            print('here')
             return False
-        print('d is ',d)
         for i in range(len(d)):
             if len(d[i]) != len(set(d[i])):
                 return False
