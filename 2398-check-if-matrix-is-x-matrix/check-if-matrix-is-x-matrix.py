505a798c5aa5d8422c1c6e5da3e75ea8909f0963
meghanakr <51442814+meghanakr7@users.noreply.github.com>
2024-03-03 23:28:24 -0500

    Time: 213 ms (75.00%) | Memory: 17.7 MB (36.73%) - LeetSync

--- 2398-check-if-matrix-is-x-matrix/check-if-matrix-is-x-matrix.py
+++ 2398-check-if-matrix-is-x-matrix/check-if-matrix-is-x-matrix.py
@@ -6,12 +6,10 @@ class Solution:
             for j in range(n):
                 if (i == j or i +j == n-1):
                     if grid[i][j] == 0:
-                        print('h1')
                         return False
                     else:
                         continue
                 elif grid[i][j] != 0:
-                    print('h3',i,j)
                     return False
         return True
 
